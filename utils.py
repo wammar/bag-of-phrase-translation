c3207703e46d2962a050e8ac56d8a4e24cf54548
Waleed Ammar <wammar@cs.cmu.edu>
2013-06-22 05:01:47 -0400

    before the report

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import cdec.score
 import io
+import sys
 
 def bleu(hyp_filename, ref_filename):
   with io.open(hyp_filename, encoding='utf8') as hyp, io.open(ref_filename, encoding='utf8') as ref:
@@ -8,6 +9,9 @@ def bleu(hyp_filename, ref_filename):
     return stats.score
 
 def dot_product(weights, features):
+  if len(weights) != len(features):
+    sys.stderr.write('\n\nnoooooooooooooooooooowaaaaaaaaaaaaaaaaaay\n\n')
+    sys.stderr.write('weights={}\nfeatures={}\n\n'.format(weights, features))
   assert(len(weights) == len(features))
   tbr = 0.0
   for k,w in weights.iteritems():
